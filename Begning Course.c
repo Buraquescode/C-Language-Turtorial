6211b7d3f09ff36aca260ae24668beca79d0afde
Buraque's Code <143602032+Buraquescode@users.noreply.github.com>
2023-11-25 21:23:35 -0800

    //
    Read the file carefully and practise all the things mentioned in this file

---
+++ Begning Course.c
@@ -0,0 +1,71 @@
+/*void main(){
+	int a,b;
+	//printf function will print you anything on console or on screen
+	printf("Hello world\n");
+    //Giving input to a console
+    
+    printf("I am Going to take input of two numbers and will add them\n");
+    printf("Enter First Number");
+	scanf("%d",&a);
+	printf("Enter Second Number");
+	scanf("%d",&b);
+	int c = a+b;
+	printf("The sum of a and b is %d",c);
+	
+	//Scanf is a function that can wrok as taking input in c language.
+	
+	
+	//Data Types
+	
+	//Int
+	//Float
+	//Char
+	//String
+	//Bool
+	
+	//We will learn 3 of them in this basic sessionn of our course.
+	
+	//int is a reserved data type that is used for taking integer numbers
+	//are we can use int for showing integers or intializing integers
+
+	
+	
+	//opreators
+	
+	//  +
+	//  -
+	//  /
+	//  *
+	//  %
+	
+*/
+void main (){
+
+	printf("Performing some opreations\n");
+	int a,b,c;
+	float d,e,f;
+	printf("Add a and b\n");
+	printf("Enter 1st Number\n");
+	scanf("%d",&a);
+	printf("Enter 2nd Number\n");
+	scanf("%d",&b);
+	c = a+b;
+	printf("The sum of c is %d\n",c);
+	
+	
+	printf("subtract a and b\n");
+	printf("Enter 1st Number\n");
+	scanf("%d",&a);
+	printf("Enter 2nd Number\n");
+	scanf("%d",&b);
+	c = a-b;
+	printf("The diffrence of c is %d\n",c);
+	
+	printf("multiplication of a and b\n");
+	printf("Enter 1st Number\n");
+	scanf("%d",&a);
+	printf("Enter 2nd Number\n");
+	scanf("%d",&b);
+	c = a*b;
+	printf("The multiple of c is %d",c);
+}
