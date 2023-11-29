e948613e7a146163c39f655bece6cde4de0b10a3
Buraque's Code <143602032+Buraquescode@users.noreply.github.com>
2023-11-29 02:27:04 -0800

    //
    Intermediate files

---
+++ Intermediate Course.c
@@ -0,0 +1,83 @@
+// if condition is used when we have sure or certain that the condition is true.
+//If else condition  is used when we have not certain about the condition.
+
+//Switch Statement is used when we have diffrent options but one choice should be ture.
+
+//Array is used when we do not have to declare more variables for diffrent number
+
+/*void main (){
+	if (20>22){
+		printf("22 is greater then 20");
+	}
+}
+*/
+/*void main(){
+	if (20>22){
+		printf("22 is less then 22");
+	}
+	else
+	printf("22 is greater then 20");
+}
+*/
+
+/*void main(){
+     int day = 2;
+	 switch(day){
+	 	case 1:
+	 		printf("Monday");
+	 		break;
+	 	case 2:
+		    printf("tuesday");
+			break;
+		case 3:
+		    printf("Weneday");
+			break;
+		case 4:
+		    printf("thursday");			
+	 }	
+}
+
+
+*/
+
+
+//Loops
+//while loop
+/*void main(){
+	int i;
+	while(i<10){
+		printf("Code Fix Lab\n");
+		i++;
+	}
+}
+*/
+
+//Do While Loop
+
+/*void main(){
+	int i;
+	do{
+		printf("SubsCribe\n");
+		i++;
+	}
+	while(i<10);
+}
+*/
+
+//Array
+
+void main (){
+	int array[]={25,26,27,28};
+	array[2]=29;
+	printf("%d\n",array[0]);
+	printf("%d",array[2]);
+}
+
+
+
+/*
+void main (){
+	char Add[]="Code Fix Lab";
+	printf("%lu\n",Add);
+}
+*/
